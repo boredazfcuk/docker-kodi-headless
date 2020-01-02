9b48ddc2d68a1dc68022c8dd3b9e6e5505eb9db1
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-01-02 21:18:01 +0000

    Manual push of any changes to stack and containers

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
-
+EXITCODE=0
 WEBSERVERUSERNAME=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<services>/{flag=1; next} /<\/services>/{flag=0} flag' | grep -o -P '(?<=\<webserverusername>).*?(?=<\/webserverusername>)')
 WEBSERVERPASSWORD=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<services>/{flag=1; next} /<\/services>/{flag=0} flag' | grep -o -P '(?<=\<webserverpassword>).*?(?=<\/webserverpassword>)')
-wget --quiet --tries=1 --user="${WEBSERVERUSERNAME}" --password="${WEBSERVERPASSWORD}" --spider "http://${HOSTNAME}:8080" || exit 1
+EXITCODE="$(wget --quiet --tries=1 --user="${WEBSERVERUSERNAME}" --password="${WEBSERVERPASSWORD}" --spider "http://${HOSTNAME}:8080" | echo ${?})"
+if [ "${EXITCODE}" != 0 ]; then
+   echo "Kodi WebUI not accessible"
+   exit 1
+fi
 
 VIDEODBSERVER=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<videodatabase>/{flag=1; next} /<\/videodatabase>/{flag=0} flag' | grep -o -P '(?<=\<host>).*?(?=<\/host>)')
 VIDEODBUSER=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<videodatabase>/{flag=1; next} /<\/videodatabase>/{flag=0} flag' | grep -o -P '(?<=\<user>).*?(?=<\/user>)')
 VIDEODBPASS=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<videodatabase>/{flag=1; next} /<\/videodatabase>/{flag=0} flag' | grep -o -P '(?<=\<pass>).*?(?=<\/pass>)')
 VIDEODBNAME=$(mysql --protocol=tcp --host "${VIDEODBSERVER}" --user "${VIDEODBUSER}" --password="${VIDEODBPASS}" --execute="show databases;" --silent | grep MyVideo)
-mysql --protocol=tcp --host "${VIDEODBSERVER}" --user "${VIDEODBUSER}" --password="${VIDEODBPASS}" --execute="use ${VIDEODBNAME}; select 1" 2>&1 >/dev/null || exit 1
+EXITCODE="$(mysql --protocol=tcp --host "${VIDEODBSERVER}" --user "${VIDEODBUSER}" --password="${VIDEODBPASS}" --execute="use ${VIDEODBNAME}; select 1" 2>&1 >/dev/null | echo ${?})"
+if [ "${EXITCODE}" != 0 ]; then
+   echo "Video database not accessible"
+   exit 1
+fi
 
 MUSICDBSERVER=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<musicdatabase>/{flag=1; next} /<\/musicdatabase>/{flag=0} flag' | grep -o -P '(?<=\<host>).*?(?=<\/host>)')
 MUSICDBUSER=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<musicdatabase>/{flag=1; next} /<\/musicdatabase>/{flag=0} flag' | grep -o -P '(?<=\<user>).*?(?=<\/user>)')
 MUSICDBPASS=$(cat "${USERDATA}/advancedsettings.xml" | awk '/<musicdatabase>/{flag=1; next} /<\/musicdatabase>/{flag=0} flag' | grep -o -P '(?<=\<pass>).*?(?=<\/pass>)')
-MUSICDBNAME=$(mysql --protocol=tcp --host "${VIDEODBSERVER}" --user "${VIDEODBUSER}" --password="${VIDEODBPASS}" --execute="show databases;" --silent | grep MyMusic)
-mysql --protocol=tcp --host "${VIDEODBSERVER}" --user "${VIDEODBUSER}" --password="${VIDEODBPASS}" --execute="use ${MUSICDBNAME}; select 1" 2>&1 >/dev/null || exit 1
+MUSICDBNAME=$(mysql --protocol=tcp --host "${MUSICDBSERVER}" --user "${MUSICDBUSER}" --password="${MUSICDBPASS}" --execute="show databases;" --silent | grep MyMusic)
+EXITCODE="$(mysql --protocol=tcp --host "${MUSICDBSERVER}" --user "${MUSICDBUSER}" --password="${MUSICDBPASS}" --execute="use ${MUSICDBNAME}; select 1" 2>&1 >/dev/null | echo ${?})"
+if [ "${EXITCODE}" != 0 ]; then
+   echo "Music database not accessible"
+   exit 1
+fi
+
+echo "Kodi WebUI, Video database and Music database all accessible"
+exit 0
